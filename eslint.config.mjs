e91ab0167149fcd963892978e8d84de8da7e08cd
wesleyara <wes1gom@gmail.com>
2025-02-10 19:49:02 -0300

    chore: change rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -61,6 +61,7 @@ export default [
       "tailwindcss/enforces-shorthand": "error",
       "tailwindcss/no-contradicting-classname": "error",
       "tailwindcss/no-unnecessary-arbitrary-value": "error",
+      "tailwindcss/no-custom-classname": "off"
     },
   },
 ];
