55688c772d7eb0dcb537a799bb849cc8aa2ea05e
wesleyara <wes1gom@gmail.com>
2025-02-10 19:56:41 -0300

    fix: add material-symbols import

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import "./styles/main.css";
+import "material-symbols";
 
 export { EchoTheme, type EchoThemeProps } from "./components/EchoTheme";
 export { EchoIcon, type EchoIconProps } from "./components/EchoIcon";
