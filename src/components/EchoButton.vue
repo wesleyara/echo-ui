c7ebc63d1998682e985d1a4108374d0600d2e755
wesleyara <wes1gom@gmail.com>
2025-02-10 19:54:01 -0300

    fix: update and adjust import e exports

--- src/components/EchoButton.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-defineProps<{ msg: string }>();
-</script>
-
-<template>
-  <button class="mt-4 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
-    {{ msg }}
-  </button>
-</template>
--- src/components/EchoTheme/index.ts
+++ src/components/EchoTheme/index.ts
@@ -1,1 +1,2 @@
 export { default as EchoTheme } from "./components/EchoTheme.vue";
+export * from "./models/echo-theme.models";
